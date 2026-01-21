edda0d60a135ed8360b58cfd7128ffabbfcb436c
Yee Mon Thant <imac5656@MacBook-Air-2.local>
2026-01-21 07:16:11 +0700

    Initial commit

---
+++ dicegame.js
@@ -0,0 +1,70 @@
+let player1Score = 0;
+let player2Score = 0;
+let player1Turn = true ;
+
+
+const message = document.getElementById('message');
+const player1ScoreBoard = document.getElementById('player1ScoreBoard');
+const player2ScoreBoard = document.getElementById('player2ScoreBoard');
+
+const player1Dice = document.getElementById('player1Dice');
+const player2Dice = document.getElementById('player2Dice');
+
+const rollBtn = document.getElementById('rollBtn');
+const resetBtn = document.getElementById('resetBtn');
+
+
+function showDisplayButton(){
+  rollBtn.style.display = 'none';
+  resetBtn.style.display = 'block';
+}
+
+rollBtn.addEventListener("click",function(){
+  const randomNumber = Math.floor(Math.random()*6)+1;
+
+  if (player1Turn){
+    player1Score += randomNumber;
+    player1ScoreBoard.textContent = player1Score;
+    player1Dice.innerHTML = randomNumber;
+    player1Dice.classList.remove("active");
+    player2Dice.classList.add("active");
+    message.textContent = "Player2 Turn";
+
+  } else {
+    player2Score += randomNumber;
+    player2ScoreBoard.textContent = player2Score;
+    player2Dice.innerHTML = randomNumber;
+    player2Dice.classList.remove("active");
+    player1Dice.classList.add("active");
+    message.textContent = "Player1 Turn";
+  }
+  if(player1Score >= 20 ){
+    message.textContent = "Player1 has won!🥳";
+    showDisplayButton();
+
+  } else if(player2Score >= 20){
+    message.textContent = "Player2 has won!🎉";
+    showDisplayButton();
+  }
+  player1Turn =!player1Turn;
+})
+
+resetBtn.addEventListener("click",function(){
+  message.textContent = "Player1 Turn";
+  player1ScoreBoard.textContent = 0;
+  player2ScoreBoard.textContent = 0;
+  player1Dice.textContent = '-';
+  player2Dice.textContent = '-';
+  player1Score = 0 ;
+  player2Score = 0 ;
+  rollBtn.style.display = 'block';
+  resetBtn.style.display = 'none';
+  player2Dice.classList.remove("active");
+  player1Dice.classList.add("active");
+ 
+
+
+
+
+})
+
